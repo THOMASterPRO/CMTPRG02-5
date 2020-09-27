756d369b502cf9ac3a7999240017b330c15b4860
THOMASter <thomas@bezemer.net>
2020-09-27 16:49:10 +0200

    started on login

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World!
+                    Hello World! Test page and stuff
                 </div>
 
                 <div class="links">
